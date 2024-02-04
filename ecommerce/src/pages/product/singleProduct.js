4b6e516008472b6bd794fa450737afc8a40f0639
samyeak-m <maharjansamyak@gmail.com>
2024-02-04 11:04:55 +0545

    cart

--- ecommerce/src/pages/product/singleProduct.js
+++ ecommerce/src/pages/product/singleProduct.js
@@ -1,96 +1,71 @@
+import { useParams } from "react-router-dom";
 import ImageGallery from "../../components/imageGallery";
 import useFetch from "../../hooks/useFetch";
-
-const product = {
-  name: "Zip Tote Basket",
-  price: "$140",
-  images: [
-    {
-      id: 1,
-      name: "Angled view",
-      src: "https://tailwindui.com/img/ecommerce-images/product-page-03-product-01.jpg",
-      alt: "Angled front view with bag zipped and handles upright."
-    },
-    {
-      id: 1,
-      name: "Angled view",
-      src: "https://tailwindui.com/img/ecommerce-images/product-page-03-product-02.jpg",
-      alt: "Angled front view with bag zipped and handles upright."
-    },
-    {
-      id: 1,
-      name: "Angled view",
-      src: "https://tailwindui.com/img/ecommerce-images/product-page-03-product-01.jpg",
-      alt: "Angled front view with bag zipped and handles upright."
-    },
-    {
-      id: 1,
-      name: "Angled view",
-      src: "https://tailwindui.com/img/ecommerce-images/product-page-03-product-01.jpg",
-      alt: "Angled front view with bag zipped and handles upright."
-    }
-  ],
-  description: `
-    <p>The Zip Tote Basket is the perfect midpoint between shopping tote and comfy backpack. With convertible straps, you can hand carry, should sling, or backpack this convenient and spacious bag. The zip top and durable canvas construction keeps your goods protected for all-day use.</p>
-  `
-};
+import Loading from "../../components/Loading";
 
 const images = [
-  "https://tailwindui.com/img/ecommerce-images/product-page-03-product-01.jpg",
-  "https://tailwindui.com/img/ecommerce-images/product-page-03-product-02.jpg",
-  "https://tailwindui.com/img/ecommerce-images/product-page-03-product-03.jpg"
+    "https://tailwindui.com/img/ecommerce-images/product-page-03-product-01.jpg",
+    "https://tailwindui.com/img/ecommerce-images/product-page-03-product-02.jpg",
+    "https://tailwindui.com/img/ecommerce-images/product-page-03-product-03.jpg"
 ];
 
 export default function SingleProduct() {
-    const {id} = useParams();
-    const { data: product,
+    const { id } = useParams();
+    const { data,
         loading,
         error,
         refetch } =
         useFetch(
-          `${process.env.REACT_APP_API_URL}/product/${id}`
+            `${process.env.REACT_APP_API_URL}/product/${id}`
         );
-  return (
-    <div className="bg-white">
-      <div className="max-w-2xl px-4 py-16 mx-auto sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <div className="lg:grid lg:grid-cols-2 lg:gap-x-8 lg:items-start">
-          <ImageGallery images={images} />
 
-          <div className="px-4 mt-10 sm:px-0 sm:mt-16 lg:mt-0">
-            <h1 className="text-3xl font-extrabold tracking-tight text-gray-900">
-              {product.name}
-            </h1>
+        const {data:product} = data||{};
+        const images = product?.images?.map((image) => process.env.REACT_APP_API_URL + "/" + image)||[];
 
-            <div className="mt-3">
-              <h2 className="sr-only">Product information</h2>
-              <p className="text-3xl text-gray-900">{product.price}</p>
+    if (loading) {
+        return <Loading />
+    }
+    return (
+        <div className="bg-white">
+            <div className="max-w-2xl px-4 py-16 mx-auto sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+                <div className="lg:grid lg:grid-cols-2 lg:gap-x-8 lg:items-start">
+                    <ImageGallery images={images} />
+
+                    <div className="px-4 mt-10 sm:px-0 sm:mt-16 lg:mt-0">
+                        <h1 className="text-3xl font-extrabold tracking-tight text-gray-900">
+                            {product?.name}
+                        </h1>
+
+                        <div className="mt-3">
+                            <h2 className="sr-only">Product information</h2>
+                            <p className="text-3xl text-gray-900">{product?.price}</p>
+                        </div>
+
+                        <div className="mt-3">
+                            <h3 className="sr-only">Reviews</h3>
+                        </div>
+
+                        <div className="mt-6">
+                            <h3 className="sr-only">Description</h3>
+
+                            <div
+                                className="space-y-6 text-base text-gray-700"
+                                dangerouslySetInnerHTML={{ __html: product?.description }}
+                            />
+                        </div>
+
+                        <div className="mt-6">
+                            <div className="flex mt-10 sm:flex-col1">
+                                <button
+                                    type="submit"
+                                    className="flex items-center justify-center flex-1 max-w-xs px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500 sm:w-full">
+                                    Add to cart
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <div className="mt-3">
-              <h3 className="sr-only">Reviews</h3>
-            </div>
-
-            <div className="mt-6">
-              <h3 className="sr-only">Description</h3>
-
-              <div
-                className="space-y-6 text-base text-gray-700"
-                dangerouslySetInnerHTML={{ __html: product.description }}
-              />
-            </div>
-
-            <div className="mt-6">
-              <div className="flex mt-10 sm:flex-col1">
-                <button
-                  type="submit"
-                  className="flex items-center justify-center flex-1 max-w-xs px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500 sm:w-full">
-                  Add to cart
-                </button>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
