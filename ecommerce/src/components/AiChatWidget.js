7d234e79d18f51ce4e8eb98bf4eec21a824925e2
samyeak-m <maharjansamyak@gmail.com>
2024-02-18 08:43:45 +0545

    openai

--- ecommerce/src/components/AiChatWidget.js
+++ ecommerce/src/components/AiChatWidget.js
@@ -6,8 +6,13 @@ function AiChatWidget() {
   const [count, setCount] = useState(0);
   const [chatWindowOpen, setChatWindowOpen] = useState(true);
   const {mutate,loading} = usePost('`${process.env.REACT_APP_API_URL}/chat`',{
-    onSuccess:()=>{},
-    onError:()=>{}
+    onSuccess:(res)=>{
+      addResponseMessage(res?.result);
+    },
+    onError:()=>{
+      addResponseMessage("Sorry, I am not available at the moment. Please try again later.");
+      setCount((c) => c + 1);
+    }
   });
 
   const handleMessage = (message) => {
